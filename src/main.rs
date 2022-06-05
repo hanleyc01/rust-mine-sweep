6b6dd454df5628cc95cb0830f07173db54f6e638
Connor Hanley <connordhanley@gmail.com>
2022-06-05 13:27:04 -0500

    Updated to main.rs
    Ran some tests, and it is working alright; I need to work on the number and flag functionality.
    
    Right now I'm trying to work out an rng which does not duplicate a list of values in itself, but I'm having a bit of reference-problems.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::{collections::HashMap};
 use rand::Rng;
+use std::collections::HashMap;
 //use eframe::egui::*;
 
+/// The `CellValue` enumerated type provides a series of descriptions which describe a particular cell on the board.
+/// Using an enumerated type allows for easy reasoning and pattern-matching.
+#[derive(Debug)]
 enum CellValue {
     Covered(bool),
     Mine(bool),
@@ -11,11 +14,12 @@ enum CellValue {
 
 /// This will be the main abstraction to represent our game board, containing
 /// all important information about the current game state.
+#[derive(Debug)]
 struct Board {
     width: u32,
     height: u32,
     difficulty: u32,
-    playing_board: HashMap<(u32, u32), [CellValue; 5]>,
+    playing_board: HashMap<(u32, u32), [CellValue; 4]>,
 }
 
 impl Board {
@@ -29,21 +33,28 @@ impl Board {
     }
 }
 
-/// Helper function which ought to intialize our playing board with three goals: 
-/// 1. Describe all cells as being `Covered(bool)`; 
+/// Helper function which ought to intialize our playing board with three goals:
+/// 1. Describe all cells as being `Covered(bool)`;
 /// 2. Randomize the placement of `Mine(bool)` within the cells;
 /// 3. Describe the cells surrounding the cells with mines with a specific `Number(u32)`, where `u32` is defined as the amount of cells
-/// nearby which contain a `Mine(true)`; 
+/// nearby which contain a `Mine(true)`;
 /// 4. set all cells to `Flag(false)`.
-fn initialize_board(width: u32, height: u32, diff: u32) -> HashMap<(u32, u32), [CellValue; 5]> {
+fn initialize_board(width: u32, height: u32, diff: u32) -> HashMap<(u32, u32), [CellValue; 4]> {
     use crate::CellValue::*;
-    let mut playing_board: HashMap<(u32, u32), [CellValue; 5]> = HashMap::new();
+    let mut playing_board: HashMap<(u32, u32), [CellValue; 4]> = HashMap::new();
 
-    let mut mine_cells: Vec<(u32, u32)>= get_random_pairs(width, height, diff);
+    let mut mine_cells: Vec<(u32, u32)> = get_random_pairs(width, height, diff);
 
     for x in 0..width {
         for y in 0..height {
-            let mut cell_descriptions: [CellValue; 5] = [Covered(true), Mine(check_identity(x, y, &mut mine_cells)), todo!(), todo!(), todo!(),];
+            let mut cell_descriptions: [CellValue; 4] = [
+                Covered(true),
+                Mine(check_identity(x, y, &mut mine_cells)),
+                Number(0),
+                Flag(false),
+            ];
+
+            playing_board.insert((x,y), cell_descriptions);
         }
     }
 
@@ -51,18 +62,39 @@ fn initialize_board(width: u32, height: u32, diff: u32) -> HashMap<(u32, u32), [
 }
 
 /// Generates a vector of `(u32, u32)` pairs which ought to contain the (x,y) coordinates of cells
-/// which have mines in them; vector should be of size `diff`, or difficulty. 
+/// which have mines in them; vector should be of size `diff`, or difficulty.
 fn get_random_pairs(width: u32, height: u32, diff: u32) -> Vec<(u32, u32)> {
     let mut mine_arr: Vec<(u32, u32)> = Vec::new();
     let mut rng = rand::thread_rng();
 
-    for x in 0..diff {
+    for i in 0..diff {
         let x = rng.gen_range(0..width);
         let y = rng.gen_range(0..height);
-        mine_arr.push((x,y));
+        mine_arr.push((x, y));
     }
 
     mine_arr
+}
+
+/// Testing out an rng which does not duplicate an of the values (relative to the `x` and `y` of the cell).
+fn non_dup_rand_pairs(width: u32, height: u32, diff: u32) -> Vec<(u32, u32)> {
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+    
+
+    let mut rng = thread_rng();
+    let mut x: Vec<u32> = (0..width).collect();
+    let mut y: Vec<u32> = (0..height).collect();
+
+    x.shuffle(&mut rng);
+    y.shuffle(&mut rng);
+
+    let mut x = x.split_at(diff as usize);
+    let mut y = y.split_at(diff as usize);
+    
+    let non_dup_pairs = x.1.into_iter().zip(y.1.into_iter()).collect::<Vec<_>>();
+
+    non_dup_pairs
 }
 
 /// Checks the head of the list of randomly generated mines in the playing field against the x and y
@@ -70,7 +102,7 @@ fn get_random_pairs(width: u32, height: u32, diff: u32) -> Vec<(u32, u32)> {
 fn check_identity(x: u32, y: u32, mines: &mut Vec<(u32, u32)>) -> bool {
     let head = &mines[0];
 
-    if  (head.0, head.1) == (x,y) {
+    if (head.0, head.1) == (x, y) {
         mines.remove(0);
         true
     } else {
@@ -79,7 +111,13 @@ fn check_identity(x: u32, y: u32, mines: &mut Vec<(u32, u32)>) -> bool {
 }
 
 fn main() {
-    let mine_cells: Vec<(u32, u32)>= get_random_pairs(10, 10, 5);
 
-    print!("{:?}", mine_cells);
+    let x = initialize_board(10, 10, 5);
+    
+    for x_h in 0..10 {
+        for y_h in 0..10{
+            println!("({}, {}) is: {:?}", x_h, y_h, x.get(&(x_h, y_h)))
+        }
+    }
+
 }
