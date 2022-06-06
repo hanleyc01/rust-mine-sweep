3c1c54f8767c1a57213e1d7df47fd7f80f48f517
Connor Hanley <connordhanley@gmail.com>
2022-06-06 15:43:48 -0500

    Updated board
    Messed around with 2d arrays more, and got stumped. 2darray syntax not what i was expecting hrnng

--- src/board.rs
+++ src/board.rs
@@ -1,19 +1,21 @@
 use crate::prelude::*;
 
-/// Main abstraction used for representing the game board; note how `game_board` is represented by constants 
+/// Main abstraction used for representing the game board; note how `game_board` is represented by constants
 /// defined in the prelude.
+#[derive(Debug, PartialEq, Clone)]
 pub struct Board {
-    num_mines: u32,
-    game_board: [[Tile; WIDTH] ; HEIGHT],
+    game_board: [[Tile; HEIGHT]; WIDTH],
 }
 
 /// This is the main abstraction used to represent the cells within the playing board.
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Tile {
     num: MineProx,
     visibility: Visibility,
 }
 
 /// Describes the visibility of a specific `Tile` within the playingboard
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub(super) enum Visibility {
     Covered,
     Uncovered,
@@ -21,7 +23,8 @@ pub(super) enum Visibility {
 }
 
 /// Abstraction to represent the proximity of nearby mines in other `Tiles`, this will be ultimately calculated
-/// on an analysis of `Board`
+/// on an analysis of `Board`.
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub(super) enum MineProx {
     Zero,
     One,
@@ -32,4 +35,68 @@ pub(super) enum MineProx {
     Six,
     Seven,
     Eight,
+    Mine,
+}
+
+impl Board {
+    /// Initializes the board using the `get_random_pairs()` helper function to determine where the mines will be on
+    /// the board.
+    pub fn new() -> Self {
+        Self {
+            game_board: {
+                
+                let mines: [(usize, usize) ; NUM_MINES] = get_random_pairs();
+                
+                let mut board: [[Tile; HEIGHT]; WIDTH] = [[Tile::init_no_mine(); HEIGHT] ; WIDTH];
+                for i in 0..WIDTH {
+                    for j in 0..HEIGHT {
+                        
+                        let pair = (i,j);
+                        
+                        if mines.contains(&pair) {
+                            board[i][j] = Tile::init_mine();
+                        }
+
+                    }
+                }
+
+                board
+            },
+        }
+    }
+}
+
+impl Tile {
+    /// Initializes a Tile with no mine.
+    fn init_no_mine() -> Self {
+        Self {
+            num: MineProx::Zero,
+            visibility: Visibility::Covered,
+        }
+    }
+
+    /// Initializes a cell a mine.
+    fn init_mine() -> Self {
+        Self {
+            num: MineProx::Mine,
+            visibility: Visibility::Covered,
+        }
+    }
+}
+
+/// Helper function which generates an `array` of `(u32, u32)`, size `const NUM_MINES`,
+///  which stores the `Tile`'s which contain a mine
+fn get_random_pairs() -> [(usize, usize); NUM_MINES] {
+    let mut arr: [(usize, usize); NUM_MINES] = [(0, 0); NUM_MINES];
+
+    let mut rng = rand::thread_rng();
+
+    for i in 0..NUM_MINES {
+        let x = rng.gen_range(0..WIDTH);
+        let y = rng.gen_range(0..HEIGHT);
+
+        arr[i] = (x, y);
+    }
+
+    arr
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use crate::prelude::*;
 
 mod board;
 mod prelude {
-    pub const WIDTH: usize = 50;
-    pub const HEIGHT: usize = 75;
+    pub const WIDTH: usize = 5;
+    pub const HEIGHT: usize = 10;
+    pub const NUM_MINES: usize = 5;
     
     pub use rand::Rng;
     pub use std::collections::HashMap;
@@ -13,5 +14,7 @@ mod prelude {
 
 
 fn main() {
-    todo!();
+    let test = Board::new();
+
+    println!("{:#?}", &test);
 }
